eb6fa04b19e6b30e495bf8aabc2edc0c402bb23d
Mohammed Abdul Sofiyan <sofiyan@Mohammeds-MacBook-Pro.local>
2023-01-11 15:37:03 +0530

    installed pillow
    create an app in django project
    
    configured views of App and added newly installed app in settings configuration
    
    Also, added path in urls for new function of views of a new App

--- MyPortfolio/MyPortfolio/urls.py
+++ MyPortfolio/MyPortfolio/urls.py
@@ -17,9 +17,11 @@ from django.contrib import admin
 from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
+from App import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.index, name='index'),
 ]
 
 urlpatterns += static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
