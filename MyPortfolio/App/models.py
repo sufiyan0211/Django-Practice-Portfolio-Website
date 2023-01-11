5ab9f5a39722eeb70f295f76fca23468095e9c1e
Mohammed Abdul Sofiyan <sofiyan@Mohammeds-MacBook-Pro.local>
2023-01-12 05:25:16 +0530

    Updated Profile Pic
    Create Model Class named it Project and also register in admin

--- MyPortfolio/App/models.py
+++ MyPortfolio/App/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Project(models.Model):
+    title = models.CharField(max_length=80)
+    description = models.CharField(max_length=250)
+    image = models.ImageField(upload_to='images/')
+
+    def __str__(self):
+        return self.title
