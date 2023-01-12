62fc3dede06708561f550a3e22759e8c116af21f
Mohammed Abdul Sofiyan <sofiyan@Mohammeds-MacBook-Pro.local>
2023-01-12 15:03:39 +0530

    Updated the Project model
    Make views for Project Model
    
    Add dummy Project in database

--- MyPortfolio/App/models.py
+++ MyPortfolio/App/models.py
@@ -3,9 +3,10 @@ from django.db import models
 # Create your models here.
 
 class Project(models.Model):
-    title = models.CharField(max_length=80)
-    description = models.CharField(max_length=250)
+    title = models.CharField(max_length=200)
+    description = models.CharField(max_length=500)
     image = models.ImageField(upload_to='images/')
+    url = models.URLField(blank=True)
 
     def __str__(self):
         return self.title
--- MyPortfolio/App/views.py
+++ MyPortfolio/App/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from App.models import Project
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    projects = Project.objects.all()
+    context ={
+        'projects':projects
+    }
+    return render(request, 'index.html', context)
